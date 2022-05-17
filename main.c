adf82f4ee31281068848c15f1f4623a26b37a768
Aldyoga <103398496+Aldyoga@users.noreply.github.com>
2022-05-16 20:05:51 -0700

    punyaAldi

--- main.c
+++ main.c
@@ -6,5 +6,6 @@ int main()
     printf("Aditya Dwi Agustino_013\n");
     printf("Akhmad Ais Mukhyi Yazid Fahmi_07\n");
     printf("Hary Dwi Kurnia Widjaya_18\n");
+    printf("Aldi Yoga Pratama_014\n");
     return 0;
 }
